31e1f08a5d3b9034e4afd4218d403d5ef767c1c7
Mirsad Sehovic <mirsad25@gmail.com>
2015-11-08 16:29:57 +0100

    Update Controller.php

--- Controller.php
+++ Controller.php
@@ -21,10 +21,10 @@ class Controller
 	public function DoControll()
 	{
 		$contact = new Contact();
-    	$gallery = new Gallery();
-    	$unik = new Unik();
-        $premium = new Premium();
-        $email = new PHPMailer;
+    		$gallery = new Gallery();
+    		$unik = new Unik();
+        	$premium = new Premium();
+		$email = new PHPMailer;
 	   
 		$xhtml="";
 		$menu = new menu();
@@ -44,40 +44,40 @@ class Controller
 			 
     	//Order BAS CV
     	if($gallery->isSubmitPressed()==true  && $gallery->getCustomerName() != "" && $gallery->getCustomerMail() != "" &&  $gallery->attachment() != null && $gallery->attachment2() != null  )
-		{	
+	{	
 		    
-		    $file= $gallery->attachment();
+		$file= $gallery->attachment();
 	        $file2 = $gallery->attachment2();
-		    $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name']));
-		    $uploadfile2 = tempnam(sys_get_temp_dir(), sha1($file2['name']));
+		$uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name']));
+		$uploadfile2 = tempnam(sys_get_temp_dir(), sha1($file2['name']));
 		 
     		if(move_uploaded_file( $file['tmp_name'], $uploadfile) && move_uploaded_file($file2['tmp_name'], $uploadfile2 )) {
     	
-				$email->IsSMTP();  // telling the class to use SMTP
-				$email->Host     = "mailout.one.com"; // SMTP server	
-		 		$fromName = $gallery->getCustomerName();
+			$email->IsSMTP();  // telling the class to use SMTP
+			$email->Host     = "mailout.one.com"; // SMTP server	
+		 	$fromName = $gallery->getCustomerName();
 		        $fromMail = $gallery->getCustomerMail();
 		        $selectedCV = $gallery->getChosenCv();
-				$email->From = 	$fromMail;
-				$email->FromName = $fromName;
-				$email->Subject = 'BAS CV';
-				$email->Body = "Hej jag skulle vilja beställa ".$selectedCV;
-				$email->addAddress('order@cvsmedjan.se');
-				$email->addAttachment($uploadfile, $file['name']);
-				$email->addAttachment($uploadfile2, $file2['name']);
-				$email->isHTML(true);
+			$email->From = 	$fromMail;
+			$email->FromName = $fromName;
+			$email->Subject = 'BAS CV';
+			$email->Body = "Hej jag skulle vilja beställa ".$selectedCV;
+			$email->addAddress('order@cvsmedjan.se');
+			$email->addAttachment($uploadfile, $file['name']);
+			$email->addAttachment($uploadfile2, $file2['name']);
+			$email->isHTML(true);
 				
-				//Meddela användaren om en beställning har gjort eller om något har gått fel.
-			    if(!$email->send()){
+			//Meddela användaren om en beställning har gjort eller om något har gått fel.
+			if(!$email->send()){
 			    	 	
 			    	 header("Location:index.php?page=error");
 			    	 exit;	   			    
-				}
+			}
     			else{
     			     	
-    			     header("Location:index.php?page=thankyou");
-    			     exit;
-				}
+    				header("Location:index.php?page=thankyou");
+    			     	exit;
+			}
 		    }
 		}
 
@@ -158,13 +158,13 @@ class Controller
     	 
     	}
     
-    	$blog = new Blog();
+    		$blog = new Blog();
 		$blogID = $blog->GetBlogID();
 	
-    	$footer = new Footer();
-    	$xhtml.= $footer->showFooter();
+    		$footer = new Footer();
+    		$xhtml.= $footer->showFooter();
 	
-	    echo $xhtml;
+	    	echo $xhtml;
 	}
 }
 ?>
